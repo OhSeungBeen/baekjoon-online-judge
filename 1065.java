7ee74939b69e085b10ac27e9230f2dd8cc014bd3
OhSeungBeen <osb1992@naver.com>
2020-02-13 14:36:51 +0900

    한수

---
+++ 1065.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int result = 0;
+    
+    if(n < 100) {
+      result = n;
+    } else {
+      result = 99;
+      for(int i = 100; i <= n; i++) {
+        int n1 = i % 10;
+        int n2 = i / 10 % 10;
+        int n3 = i / 100 % 10;
+        
+        if(n3 - n2 == n2 - n1)
+          result++;
+      }
+      if (n == 1000) result--;
+    }
+    System.out.println(result);
+  }
+}
