6770f5021721bac6930df143b0f6033eac126c4b
OhSeungBeen <osb1992@naver.com>
2020-02-13 19:39:20 +0900

    단어공부

---
+++ 1157.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[] alphabets = new int[26];
+    String input = br.readLine().toUpperCase();
+
+    int max = Integer.MIN_VALUE;
+    char result = '?';
+    
+    for(int i = 0; i < input.length(); i++) {
+      alphabets[input.charAt(i) - 65]++;
+      
+      if(alphabets[input.charAt(i) - 65] == max) {
+        result = '?';
+      } else if(alphabets[input.charAt(i) - 65] > max) {
+        result = input.charAt(i);
+        max = alphabets[input.charAt(i) - 65];
+      }
+    }
+    System.out.println(result);
+  }
+}
