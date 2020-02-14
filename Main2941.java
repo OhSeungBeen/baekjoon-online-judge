188db2848fc39be78126c9d8d8e20e9ae03fea5d
OhSeungBeen <osb1992@naver.com>
2020-02-14 10:01:55 +0900

    크로아티아 알파벳

---
+++ Main2941.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2941 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] croatia= {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+    
+    String input = br.readLine();
+    for(int i = 0; i < croatia.length; i++) {
+      input = input.replace(croatia[i], "a");
+    }
+    System.out.println(input.length());
+  }
+}
