e6d1aee43362061a14b14083460a0b10d9dfe60d
OhSeungBeen <osb1992@naver.com>
2020-02-14 17:00:36 +0900

    설탕 배달

---
+++ Main2839.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+
+    int bigPackage = 0;
+    int smallPackage = 0;
+
+    if(n % 5 == 0) {
+      System.out.println(n / 5);
+    }else {
+      while(n > 0) {
+        n -= 3;
+        smallPackage++;
+        if(n % 5 == 0) {
+          bigPackage = n / 5;
+          n = 0;
+          break;
+        }
+      }
+      System.out.println((n == 0) ? bigPackage + smallPackage : -1);
+    }
+  }
+}
