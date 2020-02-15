26ec47a2863351cca63b71392631ff0b89862ade
OhSeungBeen <osb1992@naver.com>
2020-02-15 20:10:44 +0900

    벌집

---
+++ Main2292.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    
+    if(n == 1) {
+      System.out.println(1);
+      return;
+    }
+    
+    int result = 1;
+    int i = 2;
+      
+    while(i <= n) {
+      i += 6 * result++;
+    }
+    System.out.println(result);
+  }
+}
