67c8427fdbac65b45bdfa98d9367cf8546efcf5f
OhSeungBeen <osb1992@naver.com>
2020-02-19 22:52:26 +0900

    피보나치 수 5

---
+++ Main10870.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  
+  static int getFibonacci(int n) {
+    if(n == 0)
+      return 0;
+    if(n == 1)
+      return 1;
+    return getFibonacci(n-1) + getFibonacci(n-2);
+  }
+  
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    System.out.println(getFibonacci(n));
+  }
+}
