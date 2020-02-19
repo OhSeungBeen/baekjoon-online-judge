65b1803aa3ba4a3a46b36c9630efc32e8a59610f
OhSeungBeen <osb1992@naver.com>
2020-02-19 22:53:06 +0900

    피보나치 수 5

--- Main10870.java
+++ Main10870.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main10870 {
   
   static int getFibonacci(int n) {
     if(n == 0)
